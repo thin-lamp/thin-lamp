21f0d267398f6a1380b8ec3a9db8dc31fe8aa177
mrbrique <locus.chan.dev@gmail.com>
2026-04-02 17:41:13 +0100

    arrays for buttons

--- ng/thin-lamp/src/app/app.component.ts
+++ ng/thin-lamp/src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  commands = [
+    { label: 'Color', icon: 'palette' },
+    { label: 'Connect', icon: 'mobile_share' },
+    { label: 'Stay awake', icon: 'owl' },
+    { label: 'Music', icon: 'music_note_2' },
+    { label: 'Info', icon: 'info_i' },
+
+  ];
   title = 'thin-lamp';
 }
