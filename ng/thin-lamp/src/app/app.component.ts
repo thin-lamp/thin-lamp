5e348a7d8a72319bfb059c4dff2c7ea7cb0e5118
mrbrique <locus.chan.dev@gmail.com>
2026-04-20 14:40:24 +0100

    update test case

--- ng/thin-lamp/src/app/app.component.ts
+++ ng/thin-lamp/src/app/app.component.ts
@@ -7,6 +7,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { UserCommandHandler } from './user-commands/user-command-handler';
+import { ToggleFullscreenCommand } from './user-commands/toggle-fullscreen-command';
+import { FullscreenService } from './shared/services/fullscreen.service';
 
 // type for one time, button rendering use 
 type UserCommand = {
@@ -40,15 +42,19 @@ export class AppComponent {
   // timer for counting idling
   private idleTimer?: any;
 
-  commands: UserCommand[] = [
-    { label: 'Color', icon: 'palette' },
-    { label: 'Connect', icon: 'mobile_share' },
-    { label: 'Fullscreen', icon: 'fullscreen' },
-    { label: 'Stay awake', icon: 'owl' },
-    { label: 'Music', icon: 'music_note_2' },
-    { label: 'Info', icon: 'info_i' },
+  commands: UserCommand[];
 
-  ];
+  constructor(private fullscreenService: FullscreenService) {
+    this.commands = [
+      { label: 'Color', icon: 'palette' },
+      { label: 'Connect', icon: 'mobile_share' },
+      { label: 'Fullscreen', icon: 'fullscreen', handler: new ToggleFullscreenCommand(this.fullscreenService) },
+      { label: 'Stay awake', icon: 'owl' },
+      { label: 'Music', icon: 'music_note_2' },
+      { label: 'Info', icon: 'info_i' },
+
+    ];
+  }
 
   onMouseMove(event: MouseEvent) {
     this.drawer?.open();
@@ -57,6 +63,7 @@ export class AppComponent {
 
   onCommandClick(cmd: UserCommand) {
     console.log(`${cmd.label} is clicked`);
+    cmd.handler?.execute();
   }
 
   private resetTimer() {
