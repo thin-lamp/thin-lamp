95f04238fe426e3612b5aa4e526cb9e46d7a92ea
mrbrique <locus.chan.dev@gmail.com>
2026-04-22 12:23:02 +0100

    add ColorPicker

---
+++ ng/thin-lamp/src/app/dialogue/color-picker/color-picker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tl-color-picker',
+  imports: [],
+  templateUrl: './color-picker.component.html',
+  styleUrl: './color-picker.component.scss'
+})
+export class ColorPickerComponent {
+
+}
