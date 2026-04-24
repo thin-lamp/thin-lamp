6adc9bda0441dad1d225d36b364c5f402ca6e45c
mrbrique <locus.chan.dev@gmail.com>
2026-04-24 18:13:22 +0100

    implement IdleTimeoutService

--- ng/thin-lamp/src/app/shared/services/idle-timeout.service.ts
+++ ng/thin-lamp/src/app/shared/services/idle-timeout.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IdleTimeoutService {
 
+  // idle time in ms
+  private readonly idleTime = 3000;
+  // timer for counting idling
+  private idleTimer?: any;
+  // emit when the idle timer expires
+  private idleSubject = new Subject<void>();
+
   constructor() { }
+
+  reset(): void {
+    this.stop();
+    // time again
+    this.idleTimer = setTimeout(() => {
+      // close the drawer if idle
+      this.idleSubject.next();
+    }, this.idleTime);
+  }
+
+  stop(): void {
+    if (this.idleTime) {
+      // clear the timer
+      clearTimeout(this.idleTimer);
+    }
+  }
+
+  onIdle(): Observable<void> {
+    return this.idleSubject.asObservable();
+  }
 }
