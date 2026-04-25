28e0731986276ef23f7b4ba96696f5b2732e2e96
mrbrique <locus.chan.dev@gmail.com>
2026-04-25 01:59:21 +0100

    stop idle timer during dialog

--- ng/thin-lamp/src/app/dialogue/color-picker/color-picker.component.ts
+++ ng/thin-lamp/src/app/dialogue/color-picker/color-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { ColorService } from '../../shared/services/color.service';
+import { IdleTimeoutService } from '../../shared/services/idle-timeout.service';
 
 
 
@@ -24,26 +25,38 @@ import { ColorService } from '../../shared/services/color.service';
   templateUrl: './color-picker.component.html',
   styleUrl: './color-picker.component.scss'
 })
-export class ColorPickerComponent {
+export class ColorPickerComponent implements OnInit {
 
 
   readonly dialogRef = inject(MatDialogRef<ColorPickerComponent>);
 
-  lampColor: string = '';
-
   readonly colorPalette: string[][];
 
+  lampColor: string = '';
+
+  previewColor: string = '';
+
+
   constructor(
-    private colorService: ColorService) {
+    private colorService: ColorService,
+    private idleService: IdleTimeoutService
+  ) {
     this.colorPalette = colorService.getColors();
+    this.previewColor = this.lampColor;
+  }
+
+  ngOnInit(): void {
+    this.idleService.stop();
   }
 
   onCancel() {
     this.dialogRef.close();
+    this.idleService.reset();
   }
 
   onConfirm() {
     console.log(this.lampColor);
+    this.idleService.reset();
   }
 
   get columns(): number {
